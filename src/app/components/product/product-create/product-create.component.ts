0506b17835cc71effa3eb68a556c6455c7ad8aea
Jucelio <juceliosb@gmail.com>
2022-01-06 14:13:54 -0300

    criando fumção CreaProduct

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,4 +1,7 @@
+import { Router } from '@angular/router';
+import { ProductService } from '../product.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-product-create',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productService: ProductService, private router: Router) { }
 
   ngOnInit(): void {
+    
   }
 
+  createProduct(): void{
+    this.productService.showMessage('Produto criado!')
+  }
+  
+  cancel(): void{
+    this.router.navigate(['/products'])
+  }
+  
 }
